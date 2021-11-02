fa4de89270dce7b5e1649fbfa813550c50a6d71d
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-02 21:07:20 +0500

    Add list of books along with remove button

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,19 @@
 const Books = () => {
-  return <h1>Books</h1>;
+  return (
+    <section>
+      <ul>
+        <li>Seven Habits of Highly Effective People ~ Don't Know</li>
+        <button type="button">Remove</button>
+        <li>Rich Dad, Poor Dad ~ Don't Know</li>
+        <button type="button">Remove</button>
+      </ul>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="submit" value="Submit" />
+      </form>
+    </section>
+  );
 };
 
 export default Books;
