d34ac5ccccd3e00823b5126aeb2a5d57aceaabed
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-04 01:41:20 +0500

    Update CreateNewBook component

--- src/components/books/CreateNewBook.js
+++ src/components/books/CreateNewBook.js
@@ -1,9 +1,43 @@
-const CreateNewBook = () => (
-  <form>
-    <input type="text" />
-    <input type="text" />
-    <input type="submit" value="Submit" />
-  </form>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
+
+const CreateNewBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <form onSubmit={submitBookToStore}>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Title"
+      />
+      <input
+        type="text"
+        name="author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        placeholder="Author"
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
 
 export default CreateNewBook;
