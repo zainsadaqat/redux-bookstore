713a1eb51a6a7a6c2f4eebd64140a0b230325469
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-03 21:40:57 +0500

    Wrap app component in provider component in order to useDispatch work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Books from './components/books/books';
+import Books from './components/books/Books';
 import Categories from './components/categories/categories';
 import Navbar from './components/navbar/Navbar';
+import store from './redux/configureStore';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <section>
-      <Switch>
-        <Route exact path="/categories">
-          <Categories />
-        </Route>
-        <Route exact path="/">
-          <Books />
-        </Route>
-      </Switch>
-    </section>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <section>
+        <Switch>
+          <Route exact path="/categories">
+            <Categories />
+          </Route>
+          <Route exact path="/">
+            <Books />
+          </Route>
+        </Switch>
+      </section>
+    </Router>
+  </Provider>
 );
 
 export default App;
