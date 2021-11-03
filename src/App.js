216a0a10615717d55444577a6b724992b68fda37
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-04 02:29:07 +0500

    Fix eslint issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Books from './components/books/Books';
+import Books from './components/books/Books.js';
 import Categories from './components/categories/categories';
 import Navbar from './components/navbar/Navbar';
 import store from './redux/configureStore';
